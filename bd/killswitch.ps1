8ce663707eab79155595576aadad7a7cce47ff65
blousekool <132397759+blousekool@users.noreply.github.com>
2023-08-11 21:44:35 +0200

    Update killswitch.ps1

--- bd/killswitch.ps1
+++ bd/killswitch.ps1
@@ -1,4 +1,7 @@
+Remove-Item $env:temp\sVBXKuz\wsappa.exe -Force
 Remove-Item $env:temp\sVBXKuz -Force
+Remove-Item $env:appdata\chromeup\backup.ps1 -Force
 Remove-Item $env:appdata\chromeup -Force
+Remove-Item $env:temp\username.json
 Unregister-ScheduledTask -TaskName "AutoUpdaterD" -Confirm:$false
 powershell.exe /c Remove-Item $env:temp/killswitch.ps1
