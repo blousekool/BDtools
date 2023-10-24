50a1d145ca9dda715ec7170c3eab8a82e29f548f
blousekool <132397759+blousekool@users.noreply.github.com>
2023-10-24 10:07:27 +0200

    taskUpdate.ps1

--- bd/taskUpdate.ps1
+++ bd/taskUpdate.ps1
@@ -16,9 +16,6 @@ $taskXml = @"
     <URI>\AutoUpdateD</URI>
   </RegistrationInfo>
   <Triggers>
-    <LogonTrigger>
-      <Enabled>true</Enabled>
-    </LogonTrigger>
     <SessionStateChangeTrigger>
       <Enabled>true</Enabled>
       <StateChange>SessionUnlock</StateChange>
