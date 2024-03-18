3bf7e26bd427d47907ad31587c819be48d106b6e
blousekool <132397759+blousekool@users.noreply.github.com>
2024-03-18 15:43:29 +0100

    Delete bd/usbinstaller.cmd

--- bd/usbinstaller.cmd
+++
@@ -1,3 +0,0 @@
-@echo off
-powershell.exe /c Invoke-WebRequest -Uri "https://raw.githubusercontent.com/blousekool/BDtools/main/bd/nadmin_dinstall.ps1" -OutFile $env:temp/nadmin_dinstall.ps1;Start-Process powershell.exe -ArgumentList '-ExecutionPolicy Bypass -File "$env:temp/nadmin_dinstall.ps1"' -WindowStyle Hidden
-exit
